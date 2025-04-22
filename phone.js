aefb756c0bf0ae306407dbef90f2412808fa3343
AbdulrhamnAhmed29 <abdulrhmanaahmed@gmail.com>
2025-04-22 02:54:31 -0700

    first commit

---
+++ phone.js
@@ -0,0 +1,20 @@
+let img = document.getElementById("img-show");
+
+let color = document.getElementById("background");
+
+
+
+
+function colors(x) {
+    color.style.background = x;
+
+
+}
+
+// this function change source img
+function change(source) {
+    img.src = source;
+
+
+}
+
